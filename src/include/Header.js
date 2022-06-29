568e36c36722268bd3602dd287c169c4f1389b1e
hyunjin.P <wldwlddl0531@gmail.com>
2022-06-29 17:39:07 +0900

    first commit

---
+++ src/include/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <div id='header'>
+                <div className='inner'>
+                    <h1><NavLink to="/">램프쇼핑</NavLink></h1>
+                    <ul>
+                        <li><NavLink to="/upload">상품등록하기</NavLink></li>
+                        <li><NavLink to="/product">상품보기</NavLink></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
